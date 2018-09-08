7408f6e215a4beacbb5066a4901a33e9a424f10d
Ilya Sakovich <me@hivokas.com>
2018-09-08 16:16:18 +0300

    Moved the views to another directory. Deleted some of them.

--- resources/views/messages.blade.php
+++
@@ -1,5 +0,0 @@
-@foreach (laraflash()->ready() as $message)
-    @component('laraflash::components.message.skins.'.config('laraflash.message_skin'), $message->toArray())
-    @endcomponent
-    <br>
-@endforeach
